9ae1fb68d30454c601b321f25565151133a75840
rickbeumers <rick_beumers1994@hotmail.com>
2015-06-16 20:10:09 +0200

    project

---
+++ Assets/butt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class butt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+
+		GUI.Button(new Rect(50, 50, 150, 40), "A button");
+
+		GUI.Button(new Rect(250, 50, 150, 40), "Another button");
+	}
+
+}
